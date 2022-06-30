f2d65f34fcffa2051aff71916429e82e3f25a836
Chelsea Holman <chel.holman@gmail.com>
2022-06-30 21:25:23 +0800

    added backend for wechat authentication

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -3,4 +3,9 @@ class Api::V1::PetsController < Api::V1::BaseController
     @pets = Pet.all
     render json: @pets #Just for testing
   end
+
+  def show
+    @pet = Pet.find(params[:id])
+    render json: @pet
+  end
 end
--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,9 +1,33 @@
 class Api::V1::SessionsController < Api::V1::BaseController
+  skip_before_action :verify_request, only: [:login]
+
   def login
-    # Recap of what needs to happen in login
-    # 1 - Rails sends the code to Tencent along with app secret and app id
-    # 2 - Tencent sends back user's openId
-    # 3 - Find user with openId in the database; if not, create a new user with this openId
-    # 4 - Send a JWT that stores user information to MP
+    user = find_user
+
+    token = jwt_encode(user_id: user.id) # put user_id in payload
+    render json: {
+      headers: { "X-USER-TOKEN" => token },
+      user: user
+    }
   end
+
+  private
+
+  def jwt_encode(payload) # generate JWT
+    JWT.encode payload, HMAC_SECRET, 'HS256'
+  end
+
+  def fetch_wx_open_id(code) # retrieve open id
+    app_id = Rails.application.credentials.dig(:wechat, :app_id)
+    app_secret = Rails.application.credentials.dig(:wechat, :app_secret)
+    url = "https://api.weixin.qq.com/sns/jscode2session?appid=#{app_id}&secret=#{app_secret}&js_code=#{code}&grant_type=authorization_code"
+    response = RestClient.get(url)
+    JSON.parse(response.body)
+  end
+
+  def find_user # find or create user
+    open_id = fetch_wx_open_id(params[:code])["openid"]
+    User.find_or_create_by(open_id: open_id)
+  end
+
 end
