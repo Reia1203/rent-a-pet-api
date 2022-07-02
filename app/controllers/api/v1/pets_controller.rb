23c29de6f626630e41c6acf2fbb19620b5ecaae4
Liya Tang <tangliya251@sohu.com>
2022-07-02 15:41:50 +0800

    modified form

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -9,4 +9,25 @@ class Api::V1::PetsController < Api::V1::BaseController
     @pet = Pet.find(params[:id])
     render json: @pet
   end
+
+  def create
+    @pet = Pet.new(pet_params)
+    @pet.user = @current_user
+    if @pet.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :bio, :species)
+  end
+
+  def render_error
+    render json: { errors: @pet.errors.full_messages },
+      status: :unprocessable_entity
+  end
 end
