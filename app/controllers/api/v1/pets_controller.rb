3c5c677e5c557f263eef48decd54c53e15df9c2b
Chelsea Holman <chel.holman@gmail.com>
2022-07-02 11:56:19 +0800

    seed file fix

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PetsController < Api::V1::BaseController
+  skip_before_action :verify_request, only: :index
   def index
     @pets = Pet.all
     render json: @pets #Just for testing
